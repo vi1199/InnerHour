c40c5e6813a38b2c97b3e9fa3e62f3bf69bb0e5e
Vinny Tom <nickel.caracara@gmail.com>
2018-10-10 00:33:54 +0530

    project initialized, react navigation library added - react native version: 0.57.2

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {
+    View
+} from 'react-native';
+
+class Home extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return(
+            <View>
+            </View>
+        )
+    }
+}
+
+export default Home;
